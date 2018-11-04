eb42447197b544a49ee364fc2cb5db556c9eea10
krisjohne <estrabillo.kristianjohn@gmail.com>
2018-11-04 01:50:26 -0700

    MVP

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ changeLevel();
 
 let recording_state = false;
 
-var noteTextarea = $('#note-textarea');
+// var noteTextarea = $('#note-textarea');
 var instructions = $('#recording-instructions');
 var notesList = $('ul#notes');
 
@@ -29,6 +29,8 @@ let myResult = $('#my-result');
 let myLevel = $('#level-display');
 let myScore = $('#score-display');
 
+let response = $('#input-single');
+
 console.log(myLevel);
 console.log(myScore);
 // let myText = document.querySelector("#my-text");
@@ -36,8 +38,8 @@ console.log(myScore);
 // let myTranscript = myText[0].innerHTML;
 
 // Get all notes from previous sessions and display them.
-var notes = getAllNotes();
-renderNotes(notes);
+// var notes = getAllNotes();
+// renderNotes(notes);
 
 
 /*-----------------------------
@@ -59,7 +61,7 @@ recognition.onresult = function(event) {
 
   // Get a transcript of what was said.
   var transcript = event.results[current][0].transcript;
-
+  console.log("TEST: " + transcript);
   // Add the current transcript to the contents of our Note.
   // There is a weird bug on mobile, where everything is repeated twice.
   // There is no official solution so far so we have to handle an edge case.
@@ -67,13 +69,14 @@ recognition.onresult = function(event) {
 
   if (!mobileRepeatBug) {
     noteContent += transcript;
-    noteTextarea.val(noteContent);
+    // noteTextarea.val(noteContent);
 
     // TEST WHERE DOES IT UPDATE?
     console.log(noteContent);
     myText[0].innerHTML = "We think you said " + noteContent;
+    response[0].innerHTML = noteContent;
 
-    noteTextarea.val(transcript);
+    // noteTextarea.val(transcript);
 
     MainGame();
   }
@@ -121,7 +124,7 @@ recognition.onerror = function(event) {
 $('#start-record-btn').on('click', function(e) {
   if (noteContent.length) {
     noteContent = '';
-    noteTextarea.val('');
+    // noteTextarea.val('');
   }
 
   if (!recording_state) {
@@ -143,9 +146,9 @@ $('#pause-record-btn').on('click', function(e) {
 });
 
 // Sync the text inside the text area with the noteContent variable.
-noteTextarea.on('input', function() {
-  noteContent = $(this).val();
-})
+// noteTextarea.on('input', function() {
+//   noteContent = $(this).val();
+// })
 
 $('#save-note-btn').on('click', function(e) {
   recognition.stop();
@@ -167,23 +170,23 @@ $('#save-note-btn').on('click', function(e) {
 })
 
 
-notesList.on('click', function(e) {
-  e.preventDefault();
-  var target = $(e.target);
-
-  // Listen to the selected note.
-  if (target.hasClass('listen-note')) {
-    var content = target.closest('.note').find('.content').text();
-    readOutLoud(content);
-  }
-
-  // Delete note.
-  if (target.hasClass('delete-note')) {
-    var dateTime = target.siblings('.date').text();
-    deleteNote(dateTime);
-    target.closest('.note').remove();
-  }
-});
+// notesList.on('click', function(e) {
+//   e.preventDefault();
+//   var target = $(e.target);
+//
+//   // Listen to the selected note.
+//   if (target.hasClass('listen-note')) {
+//     var content = target.closest('.note').find('.content').text();
+//     readOutLoud(content);
+//   }
+//
+//   // Delete note.
+//   if (target.hasClass('delete-note')) {
+//     var dateTime = target.siblings('.date').text();
+//     deleteNote(dateTime);
+//     target.closest('.note').remove();
+//   }
+// });
 
 
 
@@ -191,17 +194,17 @@ notesList.on('click', function(e) {
       Speech Synthesis
 ------------------------------*/
 
-function readOutLoud(message) {
-  var speech = new SpeechSynthesisUtterance();
-
-  // Set the text and voice attributes.
-  speech.text = message;
-  speech.volume = 1;
-  speech.rate = 1;
-  speech.pitch = 1;
-
-  window.speechSynthesis.speak(speech);
-}
+// function readOutLoud(message) {
+//   var speech = new SpeechSynthesisUtterance();
+//
+//   // Set the text and voice attributes.
+//   speech.text = message;
+//   speech.volume = 1;
+//   speech.rate = 1;
+//   speech.pitch = 1;
+//
+//   window.speechSynthesis.speak(speech);
+// }
 
 
 
@@ -209,51 +212,51 @@ function readOutLoud(message) {
       Helper Functions
 ------------------------------*/
 
-function renderNotes(notes) {
-  var html = '';
-  if (notes.length) {
-    notes.forEach(function(note) {
-      html += `<li class="note">
-        <p class="header">
-          <span class="date">${note.date}</span>
-          <a href="#" class="listen-note" title="Listen to Note">Listen to Note</a>
-          <a href="#" class="delete-note" title="Delete">Delete</a>
-        </p>
-        <p class="content">${note.content}</p>
-      </li>`;
-    });
-  } else {
-    html = '<li><p class="content">You don\'t have any notes yet.</p></li>';
-  }
-  notesList.html(html);
-}
+// function renderNotes(notes) {
+//   var html = '';
+//   if (notes.length) {
+//     notes.forEach(function(note) {
+//       html += `<li class="note">
+//         <p class="header">
+//           <span class="date">${note.date}</span>
+//           <a href="#" class="listen-note" title="Listen to Note">Listen to Note</a>
+//           <a href="#" class="delete-note" title="Delete">Delete</a>
+//         </p>
+//         <p class="content">${note.content}</p>
+//       </li>`;
+//     });
+//   } else {
+//     html = '<li><p class="content">You don\'t have any notes yet.</p></li>';
+//   }
+//   notesList.html(html);
+// }
+//
+//
+// function saveNote(dateTime, content) {
+//   localStorage.setItem('note-' + dateTime, content);
+// }
 
 
-function saveNote(dateTime, content) {
-  localStorage.setItem('note-' + dateTime, content);
-}
-
-
-function getAllNotes() {
-  var notes = [];
-  var key;
-  for (var i = 0; i < localStorage.length; i++) {
-    key = localStorage.key(i);
-
-    if (key.substring(0, 5) == 'note-') {
-      notes.push({
-        date: key.replace('note-', ''),
-        content: localStorage.getItem(localStorage.key(i))
-      });
-    }
-  }
-  return notes;
-}
-
-
-function deleteNote(dateTime) {
-  localStorage.removeItem('note-' + dateTime);
-}
+// function getAllNotes() {
+//   var notes = [];
+//   var key;
+//   for (var i = 0; i < localStorage.length; i++) {
+//     key = localStorage.key(i);
+//
+//     if (key.substring(0, 5) == 'note-') {
+//       notes.push({
+//         date: key.replace('note-', ''),
+//         content: localStorage.getItem(localStorage.key(i))
+//       });
+//     }
+//   }
+//   return notes;
+// }
+//
+//
+// function deleteNote(dateTime) {
+//   localStorage.removeItem('note-' + dateTime);
+// }
 
 function compare(user_input, key) {
   if (user_input.toLowerCase() == key.toLowerCase()) {
